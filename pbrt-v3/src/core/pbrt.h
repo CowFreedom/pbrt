8068d6f568499489455aa1c3c778db7661b0520d
Tristan S <tristan.scheidemann@live.com>
2017-08-13 13:53:03 +0200

    Extended main.cpp

---
+++ pbrt-v3/src/core/pbrt.h
@@ -0,0 +1,18 @@
+/*
+This code is inspired by "Physically Based Rendering: From Theory To Implementation"
+by Matt Pharr, Greg Humphreys, and Wenzel Jakob. 
+
+
+*/
+
+#include <type_traits>
+#include <algorithm>
+#include <cinttypes>
+#include<string>
+#include<vector>
+
+namespace pbrt {
+
+
+
+}
--- pbrt-v3/src/main/pbrt.cpp
+++ pbrt-v3/src/main/pbrt.cpp
@@ -4,8 +4,24 @@ by Matt Pharr, Greg Humphreys, and Wenzel Jakob.
 
 
 */
+#include <pbrt.h>
+#include<iostream> //remove
+
+template<class T, std::size_t N>
+static void usage(const T(&msg)[N] = nullptr) {
+	if (msg)
+		fprintf(stderr, "pbrt: %s\n\n", msg);
+
+	fprintf(stderr, R"(usage: pbrt [<options>] <filename.pbrt...>
+		
+		
+		)");
+}
 
 int main(int agrc, char* argv[]){
 
-
+	std::vector<std::string> filenames;
+	char msg[] = "";
+	usage(msg);
+	std::cin.get(); //remove
 }
