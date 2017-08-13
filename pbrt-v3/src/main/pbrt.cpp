cdf987eea9434209380b87fc119288e18e784b76
Tristan S <tristan.scheidemann@live.com>
2017-08-13 13:18:46 +0200

    New folder structure

---
+++ pbrt-v3/src/main/pbrt.cpp
@@ -0,0 +1,11 @@
+/*
+This code is inspired by "Physically Based Rendering: From Theory To Implementation"
+by Matt Pharr, Greg Humphreys, and Wenzel Jakob. 
+
+
+*/
+
+int main(int agrc, char* argv[]){
+
+
+}
