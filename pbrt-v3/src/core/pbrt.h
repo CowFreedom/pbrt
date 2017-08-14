5e9cd191905d05deaa00ad83c72103500ff63e02
Tristan S <tristan.scheidemann@live.com>
2017-08-14 12:27:00 +0200

    More changes

--- pbrt-v3/src/core/pbrt.h
+++ pbrt-v3/src/core/pbrt.h
@@ -8,11 +8,23 @@ by Matt Pharr, Greg Humphreys, and Wenzel Jakob.
 #include <type_traits>
 #include <algorithm>
 #include <cinttypes>
+#include <iostream> //um cout und cerr zu nutzen
 #include<string>
 #include<vector>
 
+
+
 namespace pbrt {
 
+	struct Options {
+		size_t nThreads = 0;
+		bool quickRender =  false;
+		bool quiet = false;
+		bool cat = false;
+		bool toPly = false;
+		std::string imgeFile;
+
+	};
 
 
 }
--- pbrt-v3/src/main/pbrt.cpp
+++ pbrt-v3/src/main/pbrt.cpp
@@ -7,21 +7,52 @@ by Matt Pharr, Greg Humphreys, and Wenzel Jakob.
 #include <pbrt.h>
 #include<iostream> //remove
 
+using pbrt::Options;
+
 template<class T, std::size_t N>
 static void usage(const T(&msg)[N] = nullptr) {
 	if (msg)
-		fprintf(stderr, "pbrt: %s\n\n", msg);
+		std::cerr << "pbrt: " << msg << "\n\n";
 
-	fprintf(stderr, R"(usage: pbrt [<options>] <filename.pbrt...>
+	std::cerr << R"(usage: pbrt[<options>] <filename.pbrt...>
+Rendering options:
+   --help                  Print this help text.
+   --nthreads <num>        Use specified number of threads for rendering.
+   --outfile <filename>    Write the final image to the given filename.
+
+Logging options:
+   --logdir <dir>          Specify directory that log files should be written to.
+                           Default: system temp directory (e.g. $TMPDIR or /tmp).
+   --logtostderr           Print all logging messages to stderr.
+   --minloglevel <num>     Log messages at or above this level (0 -> INFO,
+                           1 -> WARNING, 2 -> ERROR, 3-> FATAL). Default: 0
+   --v <verbosity>         Set VLOG verbosity.
+
+Reformatting options:
+   --cat                   Print a reformatted version of the input file(s) to
+                           standard output. Does not render an image.
+   --toply                 Print a reformatted version of the input file(s) to
+                           standard output and convert all triangle meshes to 
+                           PLY files. Does not render an image.
+
+)";	
 		
-		
-		)");
+	//std::cout << msg;
 }
 
-int main(int agrc, char* argv[]){
 
+int main(int agrc, char* argv[]){
+	Options options;
+
+	std::array<std::string, agrc> commandLine;
+
+	for (size_t i = 0; i < agrc; i++) {
+		std::cout << argv[i] << "\t HAHA"<<"\n";
+	}
+
+	std::ios::sync_with_stdio(false);
 	std::vector<std::string> filenames;
-	char msg[] = "";
-	usage(msg);
+	char msg[] = "msg";
+	//usage("Hey");
 	std::cin.get(); //remove
 }
